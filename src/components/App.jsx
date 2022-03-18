38dad3f0f1a65684fb89bfe271729d5da3f204b8
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-03-18 11:01:47 -0600

    remove local data and references to it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,9 @@
 import React from 'react'
-import data_autoland from '../data/autoland.json'
-import data_try from '../data/try.json'
-import data_central from '../data/mozilla-central.json'
-
 
 import Compare from './Compare'
 
 function App() {
-  let data = {
-    "try": [],
-    "mozilla-central": [],
-    "autoland": []
-  }
-
-  data.autoland = [...data_autoland.results]
-  data.try = [...data_try.results]
-  data['mozilla-central'] = [...data_central.results]
-
-  return (
-    <Compare data={data}/>
-  )
+    return <Compare />
 }
-
 
 export default App
