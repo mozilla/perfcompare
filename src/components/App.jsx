784741b1161648a2425aeca9c8184ee282936444
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-03-18 12:57:04 -0600

    replace protocol package with material ui packages renamed compare to searchview create dropdown and search input using material ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,20 @@ import data_autoland from '../data/autoland.json'
 import data_try from '../data/try.json'
 import data_central from '../data/mozilla-central.json'
 
-
-import Compare from './Compare'
+import SearchView from './SearchView'
 
 function App() {
-  let data = {
-    "try": [],
-    "mozilla-central": [],
-    "autoland": []
-  }
+    let data = {
+        try: [],
+        'mozilla-central': [],
+        autoland: [],
+    }
 
-  data.autoland = [...data_autoland.results]
-  data.try = [...data_try.results]
-  data['mozilla-central'] = [...data_central.results]
+    data.autoland = [...data_autoland.results]
+    data.try = [...data_try.results]
+    data['mozilla-central'] = [...data_central.results]
 
-  return (
-    <Compare data={data}/>
-  )
+    return <SearchView data={data} />
 }
-
 
 export default App
---
+++ src/components/SearchView.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { useState } from 'react'
+
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Container from '@mui/material/Container'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+
+import { repoList } from '../common/constants'
+
+export default function SearchView() {
+    const [repository, setRepository] = useState('')
+    const handleChange = (event) => {
+        setRepository(event.target.value)
+    }
+    const age = 'age'
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth='sm'>
+                <Box sx={{ width: '100%', maxWidth: 500 }}>
+                    <Typography variant='h1' component='div' gutterBottom>
+                        PerfCompare
+                    </Typography>
+                </Box>
+                <Box sx={{ width: '100%', maxWidth: 500 }}>
+                    <InputLabel id='select-repo-label'>repo</InputLabel>
+                    <Select
+                        labelId='select-repo-label'
+                        value={repository}
+                        label='Repository'
+                        size='small'
+                        sx={{ width: '25%' }}
+                        onChange={handleChange}
+                    >
+                        {repoList.length > 0 &&
+                            repoList.map((item) => (
+                                <MenuItem value={item} key={item}>
+                                    {item}
+                                </MenuItem>
+                            ))}
+                    </Select>
+                    <TextField
+                        label='Search For Revision'
+                        variant='outlined'
+                        size='small'
+                        sx={{ width: '75%' }}
+                    />
+                </Box>
+            </Container>
+        </React.Fragment>
+    )
+}
