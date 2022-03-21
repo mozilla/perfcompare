be26315ac1c482af413dfa306ffc18a30cec97b1
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-03-21 12:47:28 -0600

    renamed compare components to search components

---
+++ src/components/SearchView.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+
+import SearchInput from './SearchInput'
+
+export default function SearchView() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth='lg'>
+                <Box>
+                    <Typography variant='h1' component='div' align='center' gutterBottom>
+                        PerfCompare
+                    </Typography>
+                </Box>
+                <SearchInput />
+            </Container>
+        </React.Fragment>
+    )
+}
