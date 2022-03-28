28c101dbb053394fb80ff9ea088d571f18d717ca
Kimberly <ksereduck@mozilla.com>
2022-03-28 10:54:31 -0600

    merge master

--- src/thunks/searchThunk.js
+++ src/thunks/searchThunk.js
@@ -1,43 +1,45 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const TREEHERDER = 'https://treeherder.mozilla.org'
+const TREEHERDER = 'https://treeherder.mozilla.org';
 
 export const fetchRecentRevisions = createAsyncThunk(
-    'search/fetchRecentRevisions',
-    async (repository) => {
-        const response = await fetch(TREEHERDER + '/api/project/' + repository + '/push/')
-            .then((res) => res.json())
-            .then((res) => {
-                return res
-            })
-        return response.results
-    }
-)
+  'search/fetchRecentRevisions',
+  async (repository) => {
+    const response = await fetch(
+      TREEHERDER + '/api/project/' + repository + '/push/',
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        return res;
+      });
+    return response.results;
+  },
+);
 
 export const fetchRevisionByID = createAsyncThunk(
-    'search/fetchRevisionByID',
-    async ({ repository, search }) => {
-        const response = await fetch(
-            TREEHERDER + '/api/project/' + repository + '/push/?revision=' + search
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                return res
-            })
-        return response.results
-    }
-)
+  'search/fetchRevisionByID',
+  async ({ repository, search }) => {
+    const response = await fetch(
+      TREEHERDER + '/api/project/' + repository + '/push/?revision=' + search,
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        return res;
+      });
+    return response.results;
+  },
+);
 
 export const fetchRevisionsByAuthor = createAsyncThunk(
-    'search/fetchRevisionsByAuthor',
-    async ({ repository, search }) => {
-        const response = await fetch(
-            TREEHERDER + '/api/project/' + repository + '/push/?author=' + search
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                return res
-            })
-        return response.results
-    }
-)
+  'search/fetchRevisionsByAuthor',
+  async ({ repository, search }) => {
+    const response = await fetch(
+      TREEHERDER + '/api/project/' + repository + '/push/?author=' + search,
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        return res;
+      });
+    return response.results;
+  },
+);
