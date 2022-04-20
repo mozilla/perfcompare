e6f29649c33f9942f2d7cea640599cf108c3662c
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-04-20 13:32:02 -0600

    initial custom mozilla theme, updated snapshots

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import protocolTheme from '../theme/protocolTheme';
 import SearchView from './Search/SearchView';
 
 function App() {
   return (
     <React.Fragment key="App Component">
-      <CssBaseline />
-      <Router>
-        <Routes>
-          <Route path="/" element={<SearchView />} exact />
-          {/*  Your routes go here .. */}
-        </Routes>
-      </Router>
+      <ThemeProvider theme={protocolTheme}>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<SearchView />} exact />
+            {/*  Your routes go here .. */}
+          </Routes>
+        </Router>
+      </ThemeProvider>
     </React.Fragment>
   );
 }
