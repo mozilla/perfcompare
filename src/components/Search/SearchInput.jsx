9a66cda7ca528527a21f62023429ebeccea6e065
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-04-22 18:52:15 -0600

    hide search results when clicking outside input

--- src/components/Search/SearchInput.jsx
+++ src/components/Search/SearchInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Container from '@mui/material/Container';
 import FormControl from '@mui/material/FormControl';
@@ -9,50 +9,63 @@ import TextField from '@mui/material/TextField';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import { repoList } from '../../common/constants';
 import SearchViewHelper from '../../utils/searchViewHelper';
 import SearchResultsList from './SearchResultsList';
 
-const { handleChangeDropdown, handleChangeSearch } = SearchViewHelper;
+const { handleChangeDropdown, handleChangeSearch, handleClickOutsideInput } =
+  SearchViewHelper;
 
-function SearchInput(props) {
-  const { repository, searchResults } = props;
-  const repoList = ['try', 'autoland', 'mozilla-central'];
+class SearchInput extends Component {
+  componentDidMount() {
+    document.addEventListener('mousedown', handleClickOutsideInput);
+  }
 
-  return (
-    <Container maxWidth="md">
-      <FormControl sx={{ width: '25%' }}>
-        <InputLabel id="select-repository">repository</InputLabel>
-        <Select
-          value={repository}
-          labelId="select-repository"
-          label="repository"
-        >
-          {repoList.length > 0 &&
-            repoList.map((item) => (
-              <MenuItem
-                id={item}
-                value={item}
-                key={item}
-                onClick={handleChangeDropdown}
-              >
-                {item}
-              </MenuItem>
-            ))}
-        </Select>
-      </FormControl>
-      <TextField
-        label="Search By Revision ID or Author Email"
-        variant="outlined"
-        sx={{ width: '75%' }}
-        onChange={handleChangeSearch}
-      />
-      {searchResults.length > 0 && <SearchResultsList />}
-    </Container>
-  );
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', handleClickOutsideInput);
+  }
+
+  render() {
+    const { repository, searchIsFocused, searchResults } = this.props;
+    return (
+      <Container maxWidth="md">
+        <FormControl sx={{ width: '25%' }}>
+          <InputLabel id="select-repository">repository</InputLabel>
+          <Select
+            value={repository}
+            labelId="select-repository"
+            label="repository"
+          >
+            {repoList.length > 0 &&
+              repoList.map((item) => (
+                <MenuItem
+                  id={item}
+                  value={item}
+                  key={item}
+                  onClick={handleChangeDropdown}
+                >
+                  {item}
+                </MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+        <TextField
+          id="search-revision-input"
+          label="Search By Revision ID or Author Email"
+          variant="outlined"
+          sx={{ width: '75%' }}
+          onChange={handleChangeSearch}
+          focused={searchIsFocused}
+        />
+        {searchResults.length > 0 && searchIsFocused && <SearchResultsList />}
+      </Container>
+    );
+  }
 }
 
 SearchInput.propTypes = {
   repository: PropTypes.string.isRequired,
+  searchIsFocused: PropTypes.bool.isRequired,
   searchResults: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
@@ -77,6 +90,7 @@ SearchInput.propTypes = {
 function mapStateToProps(state) {
   return {
     repository: state.search.repository,
+    searchIsFocused: state.search.searchIsFocused,
     searchResults: state.search.searchResults,
   };
 }
--- src/utils/searchViewHelper.js
+++ src/utils/searchViewHelper.js
@@ -1,5 +1,6 @@
 import store from '../common/store';
 import {
+  updateSearchIsFocused,
   updateSearchValue,
   updateSearchResults,
   updateRepository,
@@ -32,9 +33,11 @@ const searchViewHelper = {
     // If repository is selected after search value is input, fetch search results
     if (search !== '') {
       searchViewHelper.searchByRevisionOrEmail(repository, search);
+    } else {
+      // Fetch 10 most recent revisions when repository changes
+      store.dispatch(fetchRecentRevisions(repository));
     }
-    // Fetch 10 most recent revisions when repository changes
-    store.dispatch(fetchRecentRevisions(repository));
+    store.dispatch(updateSearchIsFocused(true));
   },
 
   handleChangeSearch(event) {
@@ -49,6 +52,18 @@ const searchViewHelper = {
       console.log('Please select a repository');
     } else {
       searchViewHelper.searchByRevisionOrEmail(repository, search);
+    }
+  },
+
+  handleClickOutsideInput(event) {
+    if (
+      event.target.matches(
+        '#search-revision-input, .search-revision, .search-revision *',
+      )
+    ) {
+      store.dispatch(updateSearchIsFocused(true));
+    } else {
+      store.dispatch(updateSearchIsFocused(false));
     }
   },
 };
