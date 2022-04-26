1f0c6d7fdd48f06c533323f9c358d8c8ff649650
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-04-26 15:59:38 -0600

    created constants file for tests

---
+++ src/tests/utils/constants.js
@@ -0,0 +1,32 @@
+const testResults = [
+  {
+    id: 1,
+    revision: 'coconut',
+    author: 'johncleese@python.com',
+    push_timestamp: 42,
+    repository_id: 4,
+    revisions: [
+      {
+        revision: 'coconut',
+        author: 'johncleese@python.com',
+        comments: "you've got no arms left!",
+      },
+    ],
+  },
+  {
+    id: 2,
+    revision: 'spam',
+    author: 'ericidle@python.com',
+    push_timestamp: 42,
+    repository_id: 3,
+    revisions: [
+      {
+        revision: 'spam',
+        author: 'ericidle@python.com',
+        comments: "it's just a flesh wound",
+      },
+    ],
+  },
+];
+
+export default testResults;
