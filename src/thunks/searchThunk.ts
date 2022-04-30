f8ea9b3ed4d6c603882e799806f2d4d537b3eeba
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-04-30 01:42:57 -0600

    converted to use typescript

--- src/thunks/searchThunk.ts
+++ src/thunks/searchThunk.ts
@@ -1,28 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { treeherderBaseURL } from '../common/constants';
+import type { Repository, Revision } from '../types/state';
 
-export const fetchRecentRevisions = createAsyncThunk(
-  'search/fetchRecentRevisions',
-  async (repository, { rejectWithValue }) => {
-    let response;
-    try {
-      response = await fetch(
-        `${treeherderBaseURL}/api/project/${repository}/push/`,
-      );
-    } catch (err) {
-      return rejectWithValue(err.message);
-    }
-    const json = await response.json();
-    if (json.results.length > 0) {
-      return json.results;
-    }
+export const fetchRecentRevisions = createAsyncThunk<
+  Revision[],
+  Repository['name'],
+  { rejectValue: string }
+>('search/fetchRecentRevisions', async (repository, { rejectWithValue }) => {
+  let response;
+  try {
+    response = await fetch(
+      `${treeherderBaseURL}/api/project/${repository}/push/`,
+    );
+  } catch (err) {
+    return rejectWithValue((err as Error).message);
+  }
+  const json = await response.json();
+  if (json.results.length > 0) {
+    return json.results;
+  }
 
-    return rejectWithValue('No results found');
-  },
-);
+  return rejectWithValue('No results found');
+});
 
-export const fetchRevisionByID = createAsyncThunk(
+export const fetchRevisionByID = createAsyncThunk<
+  Revision[],
+  { repository: Repository['name']; search: string },
+  { rejectValue: string }
+>(
   'search/fetchRevisionByID',
   async ({ repository, search }, { rejectWithValue }) => {
     let response;
@@ -31,7 +37,7 @@ export const fetchRevisionByID = createAsyncThunk(
         `${treeherderBaseURL}/api/project/${repository}/push/?revision=${search}`,
       );
     } catch (err) {
-      return rejectWithValue(err.message);
+      return rejectWithValue((err as Error).message);
     }
     const json = await response.json();
     if (json.results.length > 0) {
@@ -42,7 +48,11 @@ export const fetchRevisionByID = createAsyncThunk(
   },
 );
 
-export const fetchRevisionsByAuthor = createAsyncThunk(
+export const fetchRevisionsByAuthor = createAsyncThunk<
+  Revision[],
+  { repository: Repository['name']; search: string },
+  { rejectValue: string }
+>(
   'search/fetchRevisionsByAuthor',
   async ({ repository, search }, { rejectWithValue }) => {
     let response;
@@ -51,7 +61,7 @@ export const fetchRevisionsByAuthor = createAsyncThunk(
         `${treeherderBaseURL}/api/project/${repository}/push/?author=${search}`,
       );
     } catch (err) {
-      return rejectWithValue(err.message);
+      return rejectWithValue((err as Error).message);
     }
     const json = await response.json();
     if (json.results.length > 0) {
--- src/utils/searchResultsHelper.ts
+++ src/utils/searchResultsHelper.ts
@@ -1,5 +1,8 @@
-const truncateHash = (item) => item.revision.slice(0, 11);
+import type { Revision } from '../types/state';
 
-const getLastRevision = (item) => item.revisions[item.revisions.length - 1];
+const truncateHash = (item: Revision) => item.revision.slice(0, 11);
+
+const getLastRevision = (item: Revision) =>
+  item.revisions[item.revisions.length - 1];
 
 export { truncateHash, getLastRevision };
