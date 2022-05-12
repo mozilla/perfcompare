10e8e4bb074ad2f46feb457c4c8712a321d53782
eng_esther <eitimielo@mozilla.com>
2022-05-12 16:33:23 -0400

    deprecate styled function

--- src/components/Search/SelectedRevisionsTable/SelectedRevisionsTable.tsx
+++ src/components/Search/SelectedRevisionsTable/SelectedRevisionsTable.tsx
@@ -5,6 +5,7 @@ import { TableRow } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import { connect, useDispatch } from 'react-redux';
@@ -12,8 +13,7 @@ import { connect, useDispatch } from 'react-redux';
 import { deleteRevision } from '../../../reducers/SelectedRevisions';
 import { Revision, State } from '../../../types/state';
 import { TableHeaderDetails } from '../../../types/tableHeaderDetails';
-import StyledTableCell from '../../Shared/StyledTableCell';
-import StyledTableRow from '../../Shared/StyledTableRow';
+// import StyledTableCell from '../../Shared/StyledTableCell';
 import './SelectedRevisionsTable.css';
 
 const tableHeaderDetails: TableHeaderDetails[] = [
@@ -36,46 +36,51 @@ function SelectedRevisionsTable(props: SelectedRevisionsProps) {
       <Table>
         <TableHead>
           <TableRow>
-            <StyledTableCell />
+            <TableCell />
             {tableHeaderDetails.map((header: TableHeaderDetails) => (
-              <StyledTableCell
+              <TableCell
                 key={header.id}
                 align={header.alignment}
                 className="title"
               >
                 {header.title}
-              </StyledTableCell>
+              </TableCell>
             ))}
-            <StyledTableCell />
           </TableRow>
         </TableHead>
         <TableBody>
           {revisions.map((row: Revision, index: number) => (
-            <StyledTableRow key={row.id}>
-              <StyledTableCell component="th" scope="row">
-                <div className="cellStyle">{index === 0 ? 'BASE' : 'NEW'}</div>
-              </StyledTableCell>
-              <StyledTableCell component="th" scope="row">
+            <TableRow key={row.id} className="rowStyle">
+              <TableCell className="cellStyle" component="th" scope="row">
+                <div className="projectCell">
+                  {index === 0 ? 'BASE' : 'NEW'}
+                </div>
+              </TableCell>
+              <TableCell className="cellStyle" component="th" scope="row">
                 {/* need to add the repository name */}
                 {row.repository_id}
-              </StyledTableCell>
-              <StyledTableCell align="left">{row.revision}</StyledTableCell>
-              <StyledTableCell align="left">{row.author}</StyledTableCell>
-              <StyledTableCell align="center">
+              </TableCell>
+              <TableCell className="cellStyle" align="left">
+                {row.revision}
+              </TableCell>
+              <TableCell className="cellStyle" align="left">
+                {row.author}
+              </TableCell>
+              <TableCell className="cellStyle" align="center">
                 {row.revisions[0].comments}
-              </StyledTableCell>
-              <StyledTableCell align="left">
+              </TableCell>
+              <TableCell className="cellStyle" align="left">
                 {row.push_timestamp}
-              </StyledTableCell>
-              <StyledTableCell align="right">
+              </TableCell>
+              <TableCell className="cellStyle" align="right">
                 <IconButton
                   id="close-button"
                   onClick={() => dispatch(deleteRevision(row.id))}
                 >
                   <Close />
                 </IconButton>
-              </StyledTableCell>
-            </StyledTableRow>
+              </TableCell>
+            </TableRow>
           ))}
         </TableBody>
       </Table>
