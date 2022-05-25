f180cfd2153d8c442e5a96d33bd7c118abd0b6f6
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-05-25 12:39:03 -0600

    create separate component for header

---
+++ src/components/Shared/PerfCompareHeader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function PerfCompareHeader() {
+  return (
+    <Box>
+      <Typography
+        variant="h1"
+        component="div"
+        align="center"
+        gutterBottom
+        className="perfcompare-header"
+      >
+        PerfCompare
+      </Typography>
+    </Box>
+  );
+}
+
+export default PerfCompareHeader;
