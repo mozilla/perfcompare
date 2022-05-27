05a193a7ffc22c092a5888c24a0ed53cccc4155d
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-05-27 14:01:50 -0600

    * added tests for checking revisions * updated fixtures file to include 5 test revisions * updated snapshots

--- src/tests/utils/fixtures.ts
+++ src/tests/utils/fixtures.ts
@@ -28,8 +28,50 @@ const getTestData = () => {
         },
       ],
     },
+    {
+      id: 3,
+      revision: 'spamspam',
+      author: 'terrygilliam@python.com',
+      push_timestamp: 42,
+      repository_id: 4,
+      revisions: [
+        {
+          revision: 'coconut',
+          author: 'terrygilliam@python.com',
+          comments: 'What, ridden on a horse?',
+        },
+      ],
+    },
+    {
+      id: 4,
+      revision: 'spamspamspamandeggs',
+      author: 'michaelpalin@python.com',
+      push_timestamp: 42,
+      repository_id: 3,
+      revisions: [
+        {
+          revision: 'spam',
+          author: 'michaelpalin@python.com',
+          comments:
+            "You've got two empty 'alves of coconuts and you're bangin' 'em togetha!",
+        },
+      ],
+    },
+    {
+      id: 5,
+      revision: 'spamspamspamspamandeggs',
+      author: 'grahamchapman@python.com',
+      push_timestamp: 42,
+      repository_id: 3,
+      revisions: [
+        {
+          revision: 'spam',
+          author: 'grahamchapman@python.com',
+          comments: 'She turned me into a newt!',
+        },
+      ],
+    },
   ];
   return { testData };
 };
-
 export default getTestData;
