eee744c20e02d5d13b4ce2984a0cdd93de32e358
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-06-10 01:34:36 -0600

    removed tests for Alert, added SnackbarProvider to test render, updated snapshots

--- src/tests/utils/test-utils.tsx
+++ src/tests/utils/test-utils.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 
 import { render as rtlRender } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
+import { SnackbarProvider } from 'notistack';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
 import { store } from '../../common/store';
+import SnackbarCloseButton from '../../components/Shared/SnackbarCloseButton';
 
 function render(ui: React.ReactElement) {
   function Wrapper({ children }: { children: React.ReactElement }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        {' '}
+        <SnackbarProvider
+          maxSnack={3}
+          autoHideDuration={6000}
+          action={(snackbarKey) => (
+            <SnackbarCloseButton snackbarKey={snackbarKey} />
+          )}
+        >
+          {children}
+        </SnackbarProvider>
+      </Provider>
+    );
   }
   return rtlRender(ui, { wrapper: Wrapper });
 }
@@ -23,7 +38,7 @@ function renderWithRouter(
 ) {
   return {
     ...render(
-      <Router location={history.location}  navigator={history}>
+      <Router location={history.location} navigator={history}>
         {ui}
       </Router>,
     ),
