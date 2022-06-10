60717a8469231cbaa575f1ac19a3f7b634c35012
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-06-10 00:14:21 -0600

    added test for results view

--- src/tests/Results/ResultsView.test.tsx
+++ src/tests/Results/ResultsView.test.tsx
@@ -1,8 +1,24 @@
 import ResultsView from '../../components/Results/ResultsView';
-import { render, screen } from '../utils/test-utils';
+import { setSelectedRevisions } from '../../reducers/SelectedRevisions';
+import getTestData from '../utils/fixtures';
+import { render, screen, store } from '../utils/test-utils';
 
-test('Should match snapshot', () => {
-  render(<ResultsView />);
+describe('Results View', () => {
+  it('Should match snapshot', () => {
+    render(<ResultsView />);
 
-  expect(screen).toMatchSnapshot();
+    expect(screen).toMatchSnapshot();
+  });
+
+  it('should display SelectedRevisionsTable if there are selected revisions', async () => {
+    const { testData } = getTestData();
+
+    // start with four selected revisions
+    const selectedRevisions = testData.slice(0, 4);
+    store.dispatch(setSelectedRevisions(selectedRevisions));
+
+    render(<ResultsView />);
+
+    expect(screen.getByText("you've got no arms left!")).toBeInTheDocument();
+  });
 });
