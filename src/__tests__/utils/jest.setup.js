f7f834f2d19a3650fec9b1b42581ddf3ba4500d7
Kimberly Sereduck <kimberlythegeek@gmail.com>
2022-07-15 13:55:34 -0700

    print warnings of tests that pass

---
+++ src/__tests__/utils/jest.setup.js
@@ -0,0 +1,11 @@
+const CONSOLE_FAIL_TYPES = ['error', 'warn'];
+
+// Throw errors when a `console.error` or `console.warn` happens
+// by overriding the functions
+CONSOLE_FAIL_TYPES.forEach((type) => {
+  console[type] = (message) => {
+    throw new Error(
+      `Failing due to console.${type} while running test!\n\n${message}`,
+    );
+  };
+});
