93556716af6e8a6cfea5a2935da42b4e8fe3d18b
Carla Severe <csevere@mozilla.com>
2023-03-09 18:46:54 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,40 +4,40 @@ module.exports = {
     browser: true,
     node: true,
   },
-  plugins: ["import", "react"],
+  plugins: ['import', 'react'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:jest/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jest/recommended',
+    'prettier',
   ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   rules: {
-    "import/order": [
-      "error",
+    'import/order': [
+      'error',
       {
-        groups: ["builtin", "external", "internal"],
+        groups: ['builtin', 'external', 'internal'],
         pathGroups: [
           {
-            pattern: "react",
-            group: "external",
-            position: "before",
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
           },
         ],
-        pathGroupsExcludedImportTypes: ["react"],
-        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
         alphabetize: {
-          order: "asc",
+          order: 'asc',
           caseInsensitive: true,
         },
       },
@@ -46,55 +46,52 @@ module.exports = {
   overrides: [
     {
       // TypeScript linting
-      files: ["src/**/**/*.{ts,tsx}"],
-      options: {
-        singleQuote: true,
-      },
-      parser: "@typescript-eslint/parser",
+      files: ['src/**/**/*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
+        project: ['./tsconfig.json'],
       },
-      plugins: ["@typescript-eslint"],
+      plugins: ['@typescript-eslint'],
       extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "airbnb-typescript",
-        "plugin:import/typescript",
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'airbnb-typescript',
+        'plugin:import/typescript',
       ],
 
       rules: {
         // https://github.com/typescript-eslint/typescript-eslint/issues/1824
-        "@typescript-eslint/indent": "off",
+        '@typescript-eslint/indent': 'off',
       },
     },
     {
       // Typescript test files
-      files: ["src/__tests__/**/*.{ts,tsx}"],
-      parser: "@typescript-eslint/parser",
+      files: ['src/__tests__/**/*.{ts,tsx}'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaFeatures: { jsx: true },
         tsconfigRootDir: __dirname,
-        project: ["./tsconfig.json"],
+        project: ['./tsconfig.json'],
       },
-      plugins: ["jest"],
-      extends: ["plugin:jest/recommended", "plugin:import/typescript"],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended', 'plugin:import/typescript'],
       rules: {
         // TODO: update tests to not use store directly and remove these overrides
         // https://github.com/mozilla/perfcompare/issues/115
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
         // test dependencies should only exist in devDependencies
-        "import/no-extraneous-dependencies": "off",
+        'import/no-extraneous-dependencies': 'off',
       },
     },
     {
       // JavaScript linting
-      files: ["src/**/*.js", "webpack/**/*.js"],
-      parser: "espree",
+      files: ['src/**/*.js', 'webpack/**/*.js'],
+      parser: 'espree',
       parserOptions: {
         ecmaVersion: 12,
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
   ],
