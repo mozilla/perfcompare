efcb0092e997f9c40e588b7662c40b7198fc6cfd
bukobab99 <viclaty@gmail.com>
2023-03-11 20:41:52 +0100

    Confidence tooltip breaking UI and SVG loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,8 +3,17 @@ const { merge } = require('webpack-merge')
 const commonConfig = require('./webpack.common.js')
 
 module.exports = (envVars) => {
-  const { env } = envVars
-  const envConfig = require(`./webpack.${env}.js`)
-  const config = merge(commonConfig, envConfig)
-  return config
+  const { env } = envVars
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, envConfig, {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
+      ],
+    },
+  })
+  return config
 }
