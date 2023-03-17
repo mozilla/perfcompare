c8402be0e1bbf8e0859dd2a38d929c2ca4be2c39
Businge931 <jimmybusinge9@gmail.com>
2023-03-17 15:31:45 +0300

    feat:(show list onselect from dropdown)mozilla#372

--- src/components/Search/SearchDropdown.tsx
+++ src/components/Search/SearchDropdown.tsx
@@ -1,20 +1,24 @@
+import React from 'react';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+
 import Select from '@mui/material/Select';
+
 import { connect } from 'react-redux';
+import type { Dispatch, SetStateAction } from 'react';
 
 import { repoMap } from '../../common/constants';
 import type { RootState } from '../../common/store';
 import useHandleChangeDropdown from '../../hooks/useHandleChangeDropdown';
 
 function SearchDropdown(props: SearchDropdownProps) {
-  const { repository, view } = props;
+  const { repository, view, showList } = props;
   const { handleChangeDropdown } = useHandleChangeDropdown();
   const size = view == 'compare-results' ? 'small' : undefined;
 
   return (
-    <FormControl sx={{ width: '100%' }} size={size}>
+    <FormControl sx={{ width: '100%', marginBottom: '8px' }} size={size}>
       <InputLabel id="select-repository">repository</InputLabel>
       <Select value={repository} labelId="select-repository" label="repository">
         {Object.keys(repoMap).map((key) => (
@@ -22,7 +26,7 @@ function SearchDropdown(props: SearchDropdownProps) {
             id={repoMap[key]}
             value={repoMap[key]}
             key={repoMap[key]}
-            onClick={(e) => void handleChangeDropdown(e)}
+            onClick={(e) => void handleChangeDropdown(e, showList)}
           >
             {repoMap[key]}
           </MenuItem>
@@ -35,6 +39,7 @@ function SearchDropdown(props: SearchDropdownProps) {
 interface SearchDropdownProps {
   repository: string;
   view: 'compare-results' | 'search';
+  showList: Dispatch<SetStateAction<boolean>>;
 }
 
 function mapStateToProps(state: RootState) {
