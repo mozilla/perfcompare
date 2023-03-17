2f969968eefe0f2867ba433d5ec3599c9b4b7d6a
jayGathu <jjgathu17@gmail.com>
2023-03-17 15:04:23 +0300

    fix/add styling to search result list

--- src/components/Search/SearchResultsList.tsx
+++ src/components/Search/SearchResultsList.tsx
@@ -1,17 +1,22 @@
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
+import { useMediaQuery,Theme } from "@mui/material";
 
 import type { Revision } from '../../types/state';
 import SearchResultsListItem from './SearchResultsListItem';
 
 function SearchResultsList(props: SearchResultsListProps) {
   const { searchResults, view } = props;
+  const isSmallScreen = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+
+  const maxHeight = isSmallScreen ? "300px" : "500px";
 
   return (
     <Box
       id="search-results-list"
       sx={{
         maxWidth: '100%',
+        maxHeight: maxHeight,
         bgcolor: 'background.paper',
         border: 1,
         borderColor: 'grey.500',
@@ -22,6 +27,7 @@ function SearchResultsList(props: SearchResultsListProps) {
         '&:hover': {
           borderColor: 'text.primary',
         },
+        overflowY: "auto",
       }}
       alignItems="flex-end"
     >
