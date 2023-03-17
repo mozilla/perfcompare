22fe7f66d9d625662bb78b59d17d46cee041d3db
Businge931 <jimmybusinge9@gmail.com>
2023-03-17 15:22:47 +0300

    feat:(show list items on select from dropdown)

--- src/hooks/useHandleChangeDropdown.ts
+++ src/hooks/useHandleChangeDropdown.ts
@@ -1,18 +1,26 @@
+import React from 'react';
 import { updateRepository } from '../reducers/SearchSlice';
 import { fetchRecentRevisions } from '../thunks/searchThunk';
 import type { Repository } from '../types/state';
+
+import type { Dispatch, SetStateAction } from 'react';
+
 import { useAppDispatch } from './app';
 
 function useHandleChangeDropdown() {
   const dispatch = useAppDispatch();
 
-  const handleChangeDropdown = async (e: React.MouseEvent<HTMLLIElement>) => {
+  const handleChangeDropdown = async (
+    e: React.MouseEvent<HTMLLIElement>,
+    showList: Dispatch<SetStateAction<boolean>>,
+  ) => {
     const repository = e.currentTarget.id;
 
     dispatch(updateRepository(repository as Repository['name']));
 
     // Fetch 10 most recent revisions when repository changes
     await dispatch(fetchRecentRevisions(repository as Repository['name']));
+    showList(true);
   };
   return { handleChangeDropdown };
 }
