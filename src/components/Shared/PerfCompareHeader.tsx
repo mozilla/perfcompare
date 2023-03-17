9c8f8098811dcf23952287b9b1baaa5536b4030f
tobidosumu <dosunmumishael@gmail.com>
2023-03-17 10:38:28 +0100

    add fixed position and boxShadow

--- src/components/Shared/PerfCompareHeader.tsx
+++ src/components/Shared/PerfCompareHeader.tsx
@@ -10,6 +10,18 @@ function PerfCompareHeader() {
         align="center"
         gutterBottom
         className="perfcompare-header"
+        sx={{ 
+          backgroundColor: '#ffffff', 
+          position: 'fixed', 
+          width: '100%',
+          top: '48px', 
+          left: '50%',
+          zIndex: '2',
+          paddingTop: '10px',
+          paddingBottom: '20px',
+          transform: 'translateX(-50%)',
+          boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.10)',
+        }}
       >
         PerfCompare
       </Typography>
