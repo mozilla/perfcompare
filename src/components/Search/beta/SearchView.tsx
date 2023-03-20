b4ee574812bc2a4fee7121a93550f439208d33d8
Carla Severe <csevere@mozilla.com>
2023-03-20 19:14:57 -0400

    Revert "added beta search route"
    This reverts commit 80ed4e6c86b3edc37504f7c13c1ddf9be9dc40e5.

--- src/components/Search/beta/SearchView.tsx
+++ src/components/Search/beta/SearchView.tsx
@@ -1,12 +1,6 @@
-import { useNavigate } from 'react-router-dom';
-
 import PerfCompareHeader from '../../Shared/beta/PerfCompareHeader';
 
 function SearchViewBeta() {
-  const navigate = useNavigate();
-  navigate({
-    pathname: '/beta',
-  });
   return (
     <section className='perfcompare-body'>
       <PerfCompareHeader />
