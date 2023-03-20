2e388bf1eeb4e701c2d04387bd72bb339ded9667
Blessing <ajibolablessing3@gmail.com>
2023-03-20 07:37:33 +0100

    Improve UX design for the search results list #394

--- src/components/Shared/SelectedRevisionsTable.tsx
+++ src/components/Shared/SelectedRevisionsTable.tsx
@@ -61,6 +61,7 @@ export function SelectedRevisionsTable(props: SelectedRevisionsProps) {
               setDraggedRow={setDraggedRow}
               setDropRow={setDropRow}
               view={view}
+              
             />
           ))}
         </TableBody>
