2605d0dfc759b2c6f7bb11676ea720121e9e9e31
timmy3000 <hatimk916@gmail.com>
2023-03-21 23:53:28 +0200

    make search results list scrollable

--- src/components/Search/SearchResultsList.tsx
+++ src/components/Search/SearchResultsList.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 
@@ -7,15 +9,43 @@ import SearchResultsListItem from './SearchResultsListItem';
 function SearchResultsList(props: SearchResultsListProps) {
   const { searchResults, view } = props;
 
+  useEffect(() => {
+    const resultsList = document.getElementById('search-results-list');
+    if (resultsList) {
+      const topOfList = resultsList.offsetTop;
+      const bottomOfVisibleWindow = window.innerHeight;
+      const listHeight = bottomOfVisibleWindow - topOfList - 12;
+      resultsList.style.maxHeight = `${Math.max(listHeight, 240)}px`;
+    }
+  }, []);
+
   return (
     <Box
       id="search-results-list"
       sx={{
         maxWidth: '100%',
+        maxHeight: '100vh',
         bgcolor: 'background.paper',
         border: 1,
         borderColor: 'grey.500',
         borderRadius: '4px',
+
+        overflow: 'auto',
+        '&::-webkit-scrollbar': {
+          width: '8px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: 'grey.400',
+          borderRadius: '20px',
+        },
+        '&::-webkit-scrollbar-thumb:hover': {
+          backgroundColor: 'grey.500',
+        },
+        '&::-webkit-scrollbar-track': {
+          backgroundColor: 'grey.200',
+          borderRadius: '20px',
+        },
+
         '&:focus': {
           borderColor: 'primary.main',
         },
