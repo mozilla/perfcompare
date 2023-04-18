45e8b8970677a8f749b2c74814d2b2068e27fd4a
bacasandrei <beatrice.raluca.acasandrei@gmail.com>
2023-04-18 15:38:44 +0300

    Address change request

--- src/components/CompareResults/beta/RevisionSelect.tsx
+++ src/components/CompareResults/beta/RevisionSelect.tsx
@@ -3,6 +3,17 @@ import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import SvgIcon from '@mui/material/SvgIcon';
+import { style } from 'typestyle'; 
+
+const styles = {
+  box: style({
+    display: 'flex',
+    gap: '1px',
+  }),
+  select: style({
+    width: '300px',
+  }),
+};
 
 const revisionsOptions = [
   'All revisions',
@@ -16,13 +27,13 @@ function RevisionSelect() {
     <Box data-testid={'revision-select'}>
       <Select
         disabled // To be removed
-        sx={{ width: 200 }}
+        className={styles.select}
         defaultValue=''
         displayEmpty
         size='small'
         renderValue={(value) => {
           return (
-            <Box sx={{ display: 'flex', gap: 1 }}>
+            <Box className={styles.box}>
               <SvgIcon>
                 <SortIcon />
               </SvgIcon>
