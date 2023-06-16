6a98a379bdcb681ff1d8d184c9ba78fd03b988ad
Carla-Moz <csevere@mozilla.com>
2023-06-16 14:22:50 -0700

    comparison warning

--- src/components/Search/CompareWithBase.tsx
+++ src/components/Search/CompareWithBase.tsx
@@ -1,7 +1,9 @@
 import { useState, createRef, useEffect } from 'react';
 
 import Divider from '@mui/material/Divider';
+import { useSnackbar, VariantType } from 'notistack';
 
+import { useAppSelector } from '../../hooks/app';
 import { Strings } from '../../resources/Strings';
 import { CompareCardsStyles } from '../../styles';
 import { InputType } from '../../types/state';
@@ -11,6 +13,7 @@ const strings = Strings.components.searchDefault;
 const stringsBase = Strings.components.searchDefault.base.collapsed.base;
 const stringsRevision =
   Strings.components.searchDefault.base.collapsed.revision;
+const warning = strings.base.collapsed.warnings.comparison;
 
 interface CompareWithBaseProps {
   mode: 'light' | 'dark';
@@ -23,17 +26,30 @@ interface Expanded {
 
 function CompareWithBase({ mode }: CompareWithBaseProps) {
   const formWrapperRef = createRef<HTMLDivElement>();
+  const { enqueueSnackbar } = useSnackbar();
+  const { search } = useAppSelector((state) => state);
   const [base, setExpanded] = useState<Expanded>({
     expanded: true,
     class: 'expanded',
   });
-
   const [formHeight, setFormHeight] = useState<number>(400);
   const styles = CompareCardsStyles(mode, formHeight);
+  const baseRepository = search.base.repository;
+  const newRepository = search.new.repository;
+  const variant: VariantType = 'warning';
 
   useEffect(() => {
     setFormHeight(formWrapperRef.current?.clientHeight || formHeight);
   }, [formHeight]);
+
+  useEffect(() => {
+    if (
+      (baseRepository === 'try' && newRepository === 'mozilla-central') ||
+      (baseRepository === 'mozilla-central' && newRepository === 'try')
+    ) {
+      enqueueSnackbar(warning, { variant });
+    }
+  }, [baseRepository, newRepository]);
 
   const toggleIsExpanded = () => {
     setExpanded({
