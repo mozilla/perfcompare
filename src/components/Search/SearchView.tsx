de775e8b0b1adb34d08e1a13318a47ed3487271a
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-06-28 02:33:49 +0500

    Pass containerRef for 'SkipLink' implementation

--- src/components/Search/SearchView.tsx
+++ src/components/Search/SearchView.tsx
@@ -1,12 +1,17 @@
+import { useRef } from 'react';
+
 import type { Theme } from '@mui/material';
 import { style } from 'typestyle';
 
 import { background } from '../../styles';
+import { skipLink } from '../../styles';
+import SkipLink from '../Accessibility/SkipLink';
 import PerfCompareHeader from '../Shared/PerfCompareHeader';
 import SearchContainer from './SearchContainer';
 import SearchViewInit from './SearchViewInit';
 
 function SearchView(props: SearchViewProps) {
+  const containerRef = useRef(null);
   const { toggleColorMode, protocolTheme } = props;
   const themeMode = protocolTheme.palette.mode;
 
@@ -18,12 +23,15 @@ function SearchView(props: SearchViewProps) {
 
   return (
     <div className={styles.container}>
+      <SkipLink containerRef={containerRef}>
+        <button className={skipLink} role="link" tabIndex={1}>Skip to search</button>
+        </SkipLink>
       <PerfCompareHeader
         themeMode={themeMode}
         toggleColorMode={toggleColorMode}
       />
       <SearchViewInit />
-      <SearchContainer themeMode={themeMode} />
+      <SearchContainer containerRef={containerRef} themeMode={themeMode} />
     </div>
   );
 }
