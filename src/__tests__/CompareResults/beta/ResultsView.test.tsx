e72cdff56054e33bea5290accd68fd32033107c5
beatrice-acasandrei <69891317+beatrice-acasandrei@users.noreply.github.com>
2023-06-28 15:29:22 +0300

    Fix tsc errors (#493)

--- src/__tests__/CompareResults/beta/ResultsView.test.tsx
+++ src/__tests__/CompareResults/beta/ResultsView.test.tsx
@@ -6,9 +6,11 @@ import { renderHook, screen } from '../../utils/test-utils';
 describe('Results View', () => {
   const protocolTheme = renderHook(() => useProtocolTheme()).result.current
     .protocolTheme;
+    const toggleColorMode = renderHook(() => useProtocolTheme()).result.current
+    .toggleColorMode;  
 
   it('Should match snapshot', () => {
-    renderWithRouter(<ResultsView protocolTheme={protocolTheme} />);
+    renderWithRouter(<ResultsView protocolTheme={protocolTheme} toggleColorMode={toggleColorMode}/>);
 
     expect(screen.getByTestId('beta-version-compare-results')).toBeInTheDocument();
     expect(document.body).toMatchSnapshot();
--- src/__tests__/CompareResults/beta/SearchInput.test.tsx
+++ src/__tests__/CompareResults/beta/SearchInput.test.tsx
@@ -6,9 +6,11 @@ import { renderHook, screen } from '../../utils/test-utils';
 describe('Search by title/test name', () => {
   const protocolTheme = renderHook(() => useProtocolTheme()).result.current
   .protocolTheme;
+  const toggleColorMode = renderHook(() => useProtocolTheme()).result.current
+  .toggleColorMode;
 
   it('Should match snapshot', () => {
-    renderWithRouter(<ResultsView protocolTheme={protocolTheme}/>);
+    renderWithRouter(<ResultsView protocolTheme={protocolTheme} toggleColorMode={toggleColorMode}/>);
 
     expect(screen.getByTestId('search-by-title-test-name')).toBeInTheDocument();
     expect(document.body).toMatchSnapshot();
