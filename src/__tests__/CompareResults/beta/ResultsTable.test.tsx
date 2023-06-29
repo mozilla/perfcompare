ff11c8f79fcf9ac97cb2cd09863c9b94990322c8
esanuandra <aesanu@mozilla.com>
2023-06-29 12:50:52 +0300

    Add test coverage for tooltip callback

--- src/__tests__/CompareResults/beta/ResultsTable.test.tsx
+++ src/__tests__/CompareResults/beta/ResultsTable.test.tsx
@@ -46,4 +46,26 @@ describe('Results Table', () => {
     expect(bubbleProps[0].data.datasets[0].label).toBe('Base');
     expect(bubbleProps[1].data.datasets[0].label).toBe('New');
   });
+
+  it('Should display tooltip for single graph distrbution', async () => {
+    const user = userEvent.setup({ delay: null });
+
+    renderWithRouter(<ResultsTable themeMode={themeMode} />);
+
+    const expandButtons = screen.getAllByTestId('expand-revision-button');
+    await user.click(expandButtons[0]);
+
+    const MockedBubble = Bubble as jest.Mock;
+    const bubbleProps = MockedBubble.mock.calls.map((call) => call[0] as ChartProps);
+    const labelFunction = bubbleProps[0].options?.plugins?.tooltip?.callbacks?.label;
+    if (!labelFunction) {
+      // eslint-disable-next-line @typescript-eslint/no-throw-literal
+      throw Error;
+    }
+
+    // @ts-expect-error does not affect the test coverage
+    // consider fixing it if we change the label function in the future
+    const labelResult = labelFunction({ raw: { x: 5, y: 0, r: 10 } });
+    expect(labelResult).toBe('5 ms');
+  });
 });
