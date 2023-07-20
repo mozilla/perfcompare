d51d590c41d54da4bf56c9a2cee1c7e61f50f178
Beatrice Acasandrei <bacasandrei@mozilla.com>
2023-07-20 15:09:26 +0300

    Addressed change request

--- src/components/CompareResults/beta/RevisionRowExpandable.tsx
+++ src/components/CompareResults/beta/RevisionRowExpandable.tsx
@@ -64,7 +64,7 @@ function RevisionRowExpandable(props: RevisionRowExpandableProps) {
           confidenceText ?
             <div>
                 <div><b>Confidence</b>: {confidenceText} </div>
-              <div className={styles.note}><b>**NOTE</b>: {strings[confidenceText]} </div>
+                <div className={styles.note}><b>**Note</b>: {strings[confidenceText]} </div>
             </div> :
             <div>
                 <div><b>Confidence</b>: Not available </div>
