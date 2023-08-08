aa6d032b4692aaf12b02e86e920a9644463947e6
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-08-08 21:37:09 +0500

    Fix formating issues

--- src/__tests__/hooks/useDocsURL.test.ts
+++ src/__tests__/hooks/useDocsURL.test.ts
@@ -3,19 +3,21 @@ import { renderHook } from '@testing-library/react';
 import useDocsURL from '../../hooks/useDocsURL';
 
 describe('useDocsURL', () => {
-
-
   it('should return the correct URL for a supported perfdocs framework', () => {
     const { result } = renderHook(() => useDocsURL('TestSuite', 1));
 
-    expect(result.current.docsURL).toBe('https://firefox-source-docs.mozilla.org/testing/perfdocs/talos.html#testsuite');
+    expect(result.current.docsURL).toBe(
+      'https://firefox-source-docs.mozilla.org/testing/perfdocs/talos.html#testsuite',
+    );
     expect(result.current.isLinkSupported).toBe(true);
   });
 
   it('should return the correct URL for "devtools" framwork', () => {
     const { result } = renderHook(() => useDocsURL('AnotherTestSuite', 12));
 
-    expect(result.current.docsURL).toBe('https://firefox-source-docs.mozilla.org/devtools/tests/performance-tests-overview.html#anothertestsuite');
+    expect(result.current.docsURL).toBe(
+      'https://firefox-source-docs.mozilla.org/devtools/tests/performance-tests-overview.html#anothertestsuite',
+    );
     expect(result.current.isLinkSupported).toBe(true);
   });
 
@@ -27,9 +29,13 @@ describe('useDocsURL', () => {
   });
 
   it('should handle suite names with special characters correctly', () => {
-    const { result } = renderHook(() => useDocsURL('Suite With:Special.Characters', 12));
+    const { result } = renderHook(() =>
+      useDocsURL('Suite With:Special.Characters', 12),
+    );
 
-    expect(result.current.docsURL).toBe('https://firefox-source-docs.mozilla.org/devtools/tests/performance-tests-overview.html#suite-with-special-characters');
+    expect(result.current.docsURL).toBe(
+      'https://firefox-source-docs.mozilla.org/devtools/tests/performance-tests-overview.html#suite-with-special-characters',
+    );
     expect(result.current.isLinkSupported).toBe(true);
   });
 });
--- src/hooks/useDocsURL.ts
+++ src/hooks/useDocsURL.ts
@@ -4,40 +4,39 @@ import { frameworkMap } from '../common/constants';
 import { Framework } from '../types/types';
 
 type SupportedPerfdocsFramework = 'talos' | 'awsy' | 'devtools';
-const supportedPerfdocsFrameworks : Record<SupportedPerfdocsFramework, string> = {
+const supportedPerfdocsFrameworks: Record<SupportedPerfdocsFramework, string> =
+  {
     talos: 'talos',
     awsy: 'awsy',
     devtools: 'performance-tests-overview',
-    
   };
 
 const useDocsURL = (suite: string, framework_id: Framework['id']) => {
+  const [docsURL, setdocsURL] = useState('');
+  const [isLinkSupported, setIsLinkSupported] = useState(true);
 
-    const [docsURL, setdocsURL] = useState('');
-    const [isLinkSupported, setIsLinkSupported] = useState(true);
+  useEffect(() => {
+    const framework = frameworkMap[framework_id];
+    const supportedFramework =
+      supportedPerfdocsFrameworks[framework as SupportedPerfdocsFramework];
+    const urlReadySuite = suite.replace(/:|\s|\.|_/g, '-').toLowerCase();
 
-    useEffect(() => {
+    const baseURL = 'https://firefox-source-docs.mozilla.org';
 
-  const framework = frameworkMap[framework_id];
-  const supportedFramework = supportedPerfdocsFrameworks[framework as SupportedPerfdocsFramework];
-  const urlReadySuite =  suite.replace(/:|\s|\.|_/g, '-').toLowerCase();
-
-  const baseURL = 'https://firefox-source-docs.mozilla.org';
-
-  if (framework_id === 12 && supportedFramework) {
-      setdocsURL(`${baseURL}/devtools/tests/${supportedFramework}.html#${urlReadySuite}`);
-
-  } else if (supportedFramework) {
-    setdocsURL(`${baseURL}/testing/perfdocs/${supportedFramework}.html#${urlReadySuite}`);
-  } else {
-    setIsLinkSupported(false);
-  }
-
-}, [suite, framework_id]);
+    if (framework_id === 12 && supportedFramework) {
+      setdocsURL(
+        `${baseURL}/devtools/tests/${supportedFramework}.html#${urlReadySuite}`,
+      );
+    } else if (supportedFramework) {
+      setdocsURL(
+        `${baseURL}/testing/perfdocs/${supportedFramework}.html#${urlReadySuite}`,
+      );
+    } else {
+      setIsLinkSupported(false);
+    }
+  }, [suite, framework_id]);
 
   return { docsURL, isLinkSupported };
-  
 };
-
 
 export default useDocsURL;
