ab2b95a7126fbc24a7d31eaa0c1b65ee3118fcf6
esanuandra <aesanu@mozilla.com>
2023-08-17 17:21:08 +0300

    Populate all revisions dropdown with real data

--- src/components/CompareResults/beta/RevisionSelect.tsx
+++ src/components/CompareResults/beta/RevisionSelect.tsx
@@ -4,11 +4,13 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import SvgIcon from '@mui/material/SvgIcon';
 import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 import { style } from 'typestyle';
 
 import { useAppSelector } from '../../../hooks/app';
 import { updateComparison } from '../../../reducers/ComparisonSlice';
 import { Strings } from '../../../resources/Strings';
+import { truncateHash } from '../../../utils/helpers';
 
 const styles = {
   box: style({
@@ -23,7 +25,7 @@ const styles = {
 const allRevisionsOption =
   Strings.components.comparisonRevisionDropdown.allRevisions;
 
-const revisionsOptions = [
+const fakeRevisionsOptions = [
   allRevisionsOption,
   'bb6a5e451dac',
   '9d5066525489',
@@ -33,6 +35,19 @@ const revisionsOptions = [
 function RevisionSelect() {
   const dispatch = useDispatch();
   const { activeComparison } = useAppSelector((state) => state.comparison);
+
+  const newRevisions = useAppSelector((state) => {
+    return state.selectedRevisions.new.map((item) =>
+      truncateHash(item.revision),
+    );
+  });
+
+  const [searchParams] = useSearchParams();
+  const fakeDataParam: string | null = searchParams.get('fakedata');
+
+  const revisionsOptions = fakeDataParam
+    ? fakeRevisionsOptions
+    : [allRevisionsOption, ...newRevisions];
 
   const handlerChangeComparison = (option: string) => {
     dispatch(updateComparison({ activeComparison: option }));
