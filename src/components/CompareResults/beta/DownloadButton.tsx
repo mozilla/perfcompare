335c6341162810f0626a21c55aa757d9b2ab123f
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-08-28 12:35:54 +0500

    Implement 'formatDownloadData' function

--- src/components/CompareResults/beta/DownloadButton.tsx
+++ src/components/CompareResults/beta/DownloadButton.tsx
@@ -14,17 +14,56 @@ const styles = {
   }),
 };
 
+interface GroupedResults {
+  [key: string]: object[];
+}
+
+interface ResultObject {
+  header_name: string;
+}
+
 function DownloadButton() {
   const results = useAppSelector((state) => {
     const { data } = state.compareResults;
     return data;
   });
 
+  const formatDownloadData = (
+    data: Record<string, ResultObject[]>,
+  ): object[] => {
+    const groupNames = Object.keys(data);
+    const transformedGroups: object[] = [];
+
+    groupNames.forEach((groupName) => {
+      const groupedResults = data[groupName].reduce(
+        (grouped: GroupedResults, result: ResultObject) => {
+          if (!grouped[result.header_name]) {
+            grouped[result.header_name] = [];
+          }
+          grouped[result.header_name].push(result);
+          return grouped;
+        },
+        {},
+      );
+
+      const transformedGroupEntries = Object.keys(groupedResults).map(
+        (header_name) => ({
+          [header_name]: groupedResults[header_name],
+        }),
+      );
+
+      transformedGroups.push(...transformedGroupEntries);
+    });
+
+    return transformedGroups;
+  };
+
   return (
     <div className={styles.downloadButton}>
       <Button
+        disabled={!results}
         href={`data:text/json;charset=utf-8,${encodeURIComponent(
-          JSON.stringify(results),
+          JSON.stringify(formatDownloadData(results)),
         )}`}
         download='perf-compare.json'
       >
