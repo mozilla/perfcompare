6523d0116c9dbfd757f31144791eb0a10bb61e58
Beatrice Acasandrei <bacasandrei@mozilla.com>
2023-09-07 11:09:13 +0300

    Address change request

--- src/__tests__/CompareResults/beta/fetchCompareResults.test.tsx
+++ src/__tests__/CompareResults/beta/fetchCompareResults.test.tsx
@@ -4,6 +4,7 @@ import useProtocolTheme from '../../../theme/protocolTheme';
 import getTestData from '../../utils/fixtures';
 import { renderWithRouter, store } from '../../utils/setupTests';
 import { renderHook } from '../../utils/test-utils';
+import { screen } from '../../utils/test-utils';
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual<typeof import('react-router-dom')>('react-router-dom'),
@@ -41,6 +42,7 @@ describe('Results View/fetchCompareResults', () => {
       'https://treeherder.mozilla.org/api/perfcompare/results/?base_repository=mozilla-central&base_revision=6089e7f0fa57a29c6d080f135f65e146c34457d8&new_repository=mozilla-central&new_revision=1d5eb1343cc87a9be3dfe4b884822506ffdda7d3&framework=1&interval=86400&no_subtests=true',
     );
     expect(document.body).toMatchSnapshot();
+    expect(screen.getByTestId('results-table')).toBeInTheDocument();
   });
 
   it('State does not contain data if fetch returns no results', () => {
@@ -70,5 +72,6 @@ describe('Results View/fetchCompareResults', () => {
     );
     expect(store.getState().compareResults.data).toStrictEqual({});
     expect(document.body).toMatchSnapshot();
+    expect(screen.getByTestId('results-table')).toBeInTheDocument();
   });
 });
