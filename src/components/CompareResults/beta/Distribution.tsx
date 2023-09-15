0feeb2326b539cbdff6a15c742028ef347663a35
beatrice-acasandrei <69891317+beatrice-acasandrei@users.noreply.github.com>
2023-09-15 09:33:29 +0300

    Display the application name (#531)
    * Display the application name
    
    * Address change request

--- src/components/CompareResults/beta/Distribution.tsx
+++ src/components/CompareResults/beta/Distribution.tsx
@@ -19,28 +19,36 @@ function Distribution(props: DistributionProps) {
   const {
     base_runs: baseRuns,
     new_runs: newRuns,
+    base_app: baseApplication,
+    new_app: newApplication,
     base_median_value: baseMedian,
     new_median_value: newMedian,
     base_stddev: baseStddev,
     new_stddev: newStddev,
     base_stddev_pct: baseStddevPercent,
     new_stddev_pct: newStddevPercent,
+    base_measurement_unit: baseUnit,
+    new_measurement_unit: newUnit,
   } = result;
 
   const baseRevisionRuns = {
     name: 'Base',
     median: baseMedian,
     values: baseRuns,
+    application: baseApplication,
     stddev: baseStddev,
     stddevPercent: baseStddevPercent,
+    measurementUnit: baseUnit,
   };
 
   const newRevisionRuns = {
     name: 'New',
     median: newMedian,
     values: newRuns,
+    application: newApplication,
     stddev: newStddev,
     stddevPercent: newStddevPercent,
+    measurementUnit: newUnit,
   };
 
   return (
