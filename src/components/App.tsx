fab3020adf856694f79775a73e6853e83cfd4cec
beatrice-acasandrei <69891317+beatrice-acasandrei@users.noreply.github.com>
2023-09-15 09:40:32 +0300

    Update links to the bugzilla on the home page (#541)
    * Changed github link to bugzilla
    
    * Address change request

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import Alert from '@mui/material/Alert';
-import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Link from '@mui/material/Link';
 import { ThemeProvider } from '@mui/material/styles';
@@ -13,7 +12,6 @@ import { Banner } from '../styles/Banner';
 import useProtocolTheme from '../theme/protocolTheme';
 import ResultsView from './CompareResults/beta/ResultsView';
 import SearchView from './Search/SearchView';
-import FeedbackAlert from './Shared/FeedbackAlert';
 import SnackbarCloseButton from './Shared/SnackbarCloseButton';
 
 const strings: BannerStrings = {
@@ -59,11 +57,11 @@ function App() {
           <CssBaseline />
           <Alert className={Banner} severity='warning'>
             <div className='banner-text'>
-              {strings.text} <Link href={strings.href}>{strings.linkText}</Link>
+              {strings.text}{' '}
+              <Link href={strings.href} target='_blank'>
+                {strings.linkText}
+              </Link>
             </div>
-            <Box display='flex' justifyContent='flex-end' alignItems='flex-end'>
-              <FeedbackAlert />
-            </Box>
           </Alert>
 
           <Router>
