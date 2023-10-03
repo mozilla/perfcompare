dc3c81fc5e9e00d04c2611d1559c599d94709b7c
Sumair Qaisar <sumairqaisar992@gmail.com>
2023-10-03 14:37:22 +0500

    Fix favicon not showing on deploy (#556)

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -42,6 +42,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './public/index.html'),
+      favicon: './public/favicon.svg',
     }),
   ],
 };
