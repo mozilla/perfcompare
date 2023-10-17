0a116784812f70e64acd27679dd3f8945d462d8a
Julien Wajsberg <felash@gmail.com>
2023-10-17 14:58:03 +0200

    Use the createStore function to create the main store object

--- src/common/store.ts
+++ src/common/store.ts
@@ -15,11 +15,8 @@ const reducer = combineReducers({
   comparison,
 });
 
-export const store = configureStore({
-  reducer,
-});
-
 export const createStore = () => configureStore({ reducer });
+export const store = createStore();
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
