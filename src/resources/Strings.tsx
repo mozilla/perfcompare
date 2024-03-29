3eda177808cd338ddfaccb227aa535ad6222f308
Julien Wajsberg <felash@gmail.com>
2024-03-29 15:29:23 +0100

    Update the error string, so that it matches the new logic

--- src/resources/Strings.tsx
+++ src/resources/Strings.tsx
@@ -126,6 +126,6 @@ export const Strings = {
     },
   },
   errors: {
-    warningText: 'Search must be a 12- or 40-character hash, or email address',
+    warningText: 'Search must be an hexadecimal hash, or an email address',
   },
 };
