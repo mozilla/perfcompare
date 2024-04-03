4894701bc86e1447b46a65e1ef8c4f8f6966f97d
Beatrice Acasandrei <bacasandrei@mozilla.com>
2024-04-03 17:36:23 +0300

    Added method to compute tc params

--- src/logic/taskcluster.ts
+++ src/logic/taskcluster.ts
@@ -5,64 +5,63 @@ import { UserCredentials } from '../types/types';
 export const prodTaskclusterUrl = 'https://firefox-ci-tc.services.mozilla.com';
 export const stagingTaskclusterUrl =
   'https://stage.taskcluster.nonprod.cloudops.mozgcp.net';
+export const stagingClientId = 'perfcompare-staging-client';
 export const tcClientIdMap: Record<string, string> = {
   'https://perf.compare': 'production',
   'https://beta--mozilla-perfcompare.netlify.app': 'beta',
   'http://localhost:3000': 'localhost-3000',
-  'https://tc-staging.treeherder.nonprod.cloudops.mozgcp.net':
-    'taskcluster-staging',
 };
 
-export const tcAuthCallbackUrl = '/taskcluster-auth';
-export const clientId = `perfcompare-${
-  tcClientIdMap[window.location.origin]
-}-client`;
-export const redirectURI = `${window.location.origin}${tcAuthCallbackUrl}`;
-
-export const getRootUrl = (rootUrl: string) => {
-  // we need this workaround for the perfcompare-taskcluster-staging deployment since all repository fixtures
-  // and the default login rootUrls are for https://firefox-ci-tc.services.mozilla.com
-  if (
-    rootUrl === prodTaskclusterUrl &&
-    clientId === 'perfcompare-taskcluster-staging-client'
-  ) {
-    return stagingTaskclusterUrl;
+export const getTaskclusterParams = () => {
+  const redirectUri = `${window.location.origin}/taskcluster-auth`;
+  let tcParams = {
+    url: prodTaskclusterUrl,
+    redirectUri: redirectUri,
+    clientId: `perfcompare-${tcClientIdMap[window.location.origin]}-client`,
+  };
+  if (window.location.hash.includes('taskcluster-staging')) {
+    tcParams.url = stagingTaskclusterUrl;
+    tcParams.clientId = stagingClientId;
   }
-  return rootUrl;
+  return tcParams;
 };
 
 const generateNonce = () => {
   return window.crypto.randomUUID();
 };
 
-const getAuthCode = (taskclusterUrl: string) => {
+const getAuthCode = (tcParams: {
+  url: string;
+  redirectUri: string;
+  clientId: string;
+}) => {
   const nonce = generateNonce();
   sessionStorage.setItem('requestState', nonce); // The request state it's stored in sessionStorage so that it can be used in the callback
-  sessionStorage.setItem('taskclusterUrl', taskclusterUrl);
+  sessionStorage.setItem('taskclusterUrl', tcParams.url);
 
   const params = new URLSearchParams({
-    client_id: clientId,
+    client_id: tcParams.clientId,
     response_type: 'code',
-    redirect_uri: redirectURI,
+    redirect_uri: tcParams.redirectUri,
     scope: 'hooks:trigger-hook:*',
     state: nonce,
   });
-  const url = `${taskclusterUrl}/login/oauth/authorize/?${params.toString()}`;
+  const url = `${tcParams.url}/login/oauth/authorize/?${params.toString()}`;
   window.open(url, '_blank');
 };
 
 export const checkTaskclusterCredentials = () => {
-  const taskclusterUrl = getRootUrl(prodTaskclusterUrl);
+  const taskclusterParams = getTaskclusterParams();
   const userCredentials = JSON.parse(
     sessionStorage.getItem('userCredentials') as string,
   ) as UserCredentials;
 
   if (
     !userCredentials ||
-    !userCredentials[taskclusterUrl]
+    !userCredentials[taskclusterParams.url]
     // TODO: once the userCredentials are set in sessionStorage check if the "expires" date is in the past
   ) {
-    getAuthCode(taskclusterUrl);
+    getAuthCode(taskclusterParams);
   }
   // TODO: handle case where the user navigates directly to the login route
 };
