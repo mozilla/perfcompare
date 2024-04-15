d06464411fd4b76e940aa5ed5321f36fc889a182
esanuandra <aesanu@mozilla.com>
2024-04-15 17:07:29 +0300

    Remove console.log with token bearer

--- src/components/TaskclusterAuth/TaskclusterCallback.tsx
+++ src/components/TaskclusterAuth/TaskclusterCallback.tsx
@@ -1,12 +1,4 @@
-import { useLoaderData } from 'react-router-dom';
-
-import { LoaderReturnValue } from './loader';
-
 export default function TaskclusterCallback() {
-  const { access_token: accessToken, token_type: tokenType } =
-    useLoaderData() as LoaderReturnValue;
-  console.log('accessToken ', accessToken, 'tokenType ', tokenType);
-
   return (
     <div className='pt-5'>
       <h2 className='justify-content-center'>
