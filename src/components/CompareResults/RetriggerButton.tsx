5b9279b78f3a432822044ab10913c99c1509bb63
Julien Wajsberg <felash@gmail.com>
2024-06-27 16:56:33 +0200

    Implement the retrigger modal

--- src/components/CompareResults/RetriggerButton.tsx
+++ src/components/CompareResults/RetriggerButton.tsx
@@ -9,6 +9,11 @@ import {
   Paper,
   Typography,
   Button,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
 } from '@mui/material';
 
 import TaskclusterLogo from '../../assets/taskcluster-logo.png';
@@ -131,26 +136,80 @@ function SignInModal(props: SignInModalProps) {
   );
 }
 
+function RetriggerCountSelect({
+  prefix,
+  label,
+}: {
+  prefix: string;
+  label: string;
+}) {
+  return (
+    <FormControl sx={{ width: '100%' }}>
+      <InputLabel id={`${prefix}-retrigger-count-label`}>{label}</InputLabel>
+      <Select
+        labelId={`${prefix}-retrigger-count-label`}
+        name={`${prefix}-retrigger-count`}
+        defaultValue={0}
+        label={label}
+        sx={{ height: 32 }}
+      >
+        <MenuItem value={0}>0</MenuItem>
+        <MenuItem value={1}>1</MenuItem>
+        <MenuItem value={2}>2</MenuItem>
+        <MenuItem value={3}>3</MenuItem>
+        <MenuItem value={4}>4</MenuItem>
+        <MenuItem value={5}>5</MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
 type RetriggerModalProps = {
   open: boolean;
   onClose: () => unknown;
   onRetriggerClick: (times: { baseTimes: number; newTimes: number }) => unknown;
 };
 
-function RetriggerModal({ open, onClose }: RetriggerModalProps) {
+function RetriggerModal(props: RetriggerModalProps) {
+  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const baseCount = formData.get('base-retrigger-count');
+    const newCount = formData.get('new-retrigger-count');
+    props.onRetriggerClick({
+      baseTimes: baseCount ? +baseCount : 0,
+      newTimes: newCount ? +newCount : 0,
+    });
+  };
+
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      aria-labelledby='retrigger-modal-title'
-      sx={{ display: 'flex' }}
+    <CenteredModal
+      open={props.open}
+      onClose={props.onClose}
+      ariaLabelledby='retrigger-modal-title'
+      paperStyle={{ display: 'flex', flexDirection: 'column', gap: 2 }}
     >
-      <Box>
-        <Typography id='retrigger-modal-title' component='h2'>
-          Configure the number of retriggers
-        </Typography>
-      </Box>
-    </Modal>
+      <Typography id='retrigger-modal-title' component='h2' variant='h2'>
+        Retrigger jobs
+      </Typography>
+      <Typography>
+        Choose how many clones of the base and new tasks will be started.
+      </Typography>
+      <form onSubmit={onFormSubmit}>
+        <Grid container gap={1}>
+          <Grid item xs={3}>
+            <RetriggerCountSelect prefix='base' label='Base' />
+          </Grid>
+          <Grid item xs={3}>
+            <RetriggerCountSelect prefix='new' label='New' />
+          </Grid>
+          <Grid item xs='auto' ml='auto'>
+            <Button type='submit'>Retrigger</Button>
+          </Grid>
+        </Grid>
+      </form>
+    </CenteredModal>
   );
 }
 
