0af3c9f0d33bda7ccf691e288cfdb9d208895572
esanuandra <aesanu@mozilla.com>
2024-07-01 15:21:02 +0300

    Mock taskcluster-client-web for jest

--- src/__tests__/utils/setupTests.ts
+++ src/__tests__/utils/setupTests.ts
@@ -36,6 +36,43 @@ global.TextEncoder = TextEncoder;
 
 let store: Store;
 
+const mockBuildUrl = jest.fn((root: string, taskId: string, path: string) => {
+  return `${root}/${taskId}/artifacts/${path}`;
+});
+
+const mockTask = jest.fn(() => {
+  return {
+    tags: { test: 'test' },
+    id: 'TASKID',
+    payload: { env: { MOZHARNESS_TEST_PATHS: '{ "mock": ["mock.json"] }' } },
+  };
+});
+const mockUse = jest.fn().mockImplementation(() => {
+  return {
+    createTask: jest.fn(() => {
+      return 'ACTION_TASKID';
+    }),
+  };
+});
+
+// after upgrading taskcluster to 39.1.1, fail to `import taskcluster-client-web`
+jest.mock('taskcluster-client-web', () => {
+  return {
+    Queue: jest.fn().mockImplementation(() => {
+      return {
+        buildUrl: mockBuildUrl,
+        getLatestArtifact:
+          'https://firefox-ci-tc.services.mozilla.com/api/queue/v1/task',
+        task: mockTask,
+        use: mockUse,
+      };
+    }),
+    slugid: jest.fn(() => {
+      return 'TEST_SLUGID';
+    }),
+  };
+});
+
 jest.mock('react-chartjs-2', () => ({
   Bubble: jest.fn(),
   Line: jest.fn(),
