271f8af2df184b3e501fe95a18a0776037dbc60d
beatrice-acasandrei <69891317+beatrice-acasandrei@users.noreply.github.com>
2024-08-08 13:23:37 +0300

    Add breadcrumbs to subtests (#699)
    * Add breadcrumbs to subtests
    
    * Address comments
    
    * Address follow-up comments

--- src/components/CompareResults/SubtestsResults/SubtestsResultsMain.tsx
+++ src/components/CompareResults/SubtestsResults/SubtestsResultsMain.tsx
@@ -4,6 +4,7 @@ import { Container } from '@mui/system';
 import { Await, useLoaderData } from 'react-router-dom';
 import { style } from 'typestyle';
 
+import { subtestsView, subtestsOverTimeView } from '../../../common/constants';
 import { useAppSelector } from '../../../hooks/app';
 import { Colors, Spacing } from '../../../styles';
 import type { SubtestsRevisionsHeader } from '../../../types/state';
@@ -11,10 +12,15 @@ import DownloadButton from '.././DownloadButton';
 import SearchInput from '.././SearchInput';
 import { LoaderReturnValue } from '../subtestsLoader';
 import { LoaderReturnValue as OvertimeLoaderReturnValue } from '../subtestsOverTimeLoader';
+import SubtestsBreadcrumbs from './SubtestsBreadcrumbs';
 import SubtestsResultsTable from './SubtestsResultsTable';
 import SubtestsRevisionHeader from './SubtestsRevisionHeader';
 
-function SubtestsResultsMain() {
+type SubtestsResultsMainProps = {
+  view: typeof subtestsView | typeof subtestsOverTimeView;
+};
+
+function SubtestsResultsMain({ view }: SubtestsResultsMainProps) {
   const { results } = useLoaderData() as
     | LoaderReturnValue
     | OvertimeLoaderReturnValue;
@@ -57,6 +63,7 @@ function SubtestsResultsMain() {
     <Container className={styles.container} data-testid='subtests-main'>
       <Await resolve={results}>
         <header>
+          <SubtestsBreadcrumbs view={view} />
           <SubtestsRevisionHeader header={subtestsHeader} />
           <div className={styles.content}>
             <SearchInput onChange={setSearchTerm} />
