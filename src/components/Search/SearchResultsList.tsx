7213f81fba77b3bb17fedca6faf147324a2b2bae
Carla Severe <csevere@mozilla.com>
2024-09-23 20:46:58 -0700

    removed repeated styles

--- src/components/Search/SearchResultsList.tsx
+++ src/components/Search/SearchResultsList.tsx
@@ -24,85 +24,59 @@ interface SearchResultsListProps {
 const sharedSelectStyles = {
   borderRadius: '4px',
   marginTop: `${Spacing.xSmall}px`,
-  height: '285px',
+  maxHeight: '285px',
   overflow: 'auto',
   maxWidth: '100%',
   padding: `${Spacing.xSmall}px`,
   border: `1px solid ${Colors.BorderDefault}`,
+  zIndex: 100,
 };
 
-const SelectListLight = style({
-  backgroundColor: Colors.Background300,
-  zIndex: 100,
-  position: 'relative',
-  ...sharedSelectStyles,
-  $nest: {
-    '.MuiListItemButton-root': {
-      padding: `${Spacing.xSmall}px ${Spacing.Small}px`,
-      $nest: {
-        '&:hover': {
-          backgroundColor: Colors.SecondaryHover,
-          borderRadius: '4px',
-        },
-        '&:active': {
-          backgroundColor: Colors.SecondaryActive,
-          borderRadius: '4px',
+function getStyles(theme: string) {
+  const backgroundColor =
+    theme === 'light' ? Colors.Background300 : Colors.Background300Dark;
+  const hoverColor =
+    theme === 'light' ? Colors.SecondaryHover : Colors.SecondaryHoverDark;
+  const activeColor =
+    theme === 'light' ? Colors.SecondaryActive : Colors.SecondaryActiveDark;
+  const captionStyle =
+    theme === 'light' ? captionStylesLight : captionStylesDark;
+
+  return style({
+    backgroundColor,
+    position: 'relative',
+    ...sharedSelectStyles,
+    $nest: {
+      '.MuiListItemButton-root': {
+        padding: `${Spacing.xSmall}px ${Spacing.Small}px`,
+        $nest: {
+          '&:hover': {
+            backgroundColor: hoverColor,
+            borderRadius: '4px',
+          },
+          '&:active': {
+            backgroundColor: activeColor,
+            borderRadius: '4px',
+          },
         },
       },
-    },
-    '.item-selected': {
-      backgroundColor: Colors.SecondaryHover,
-      borderRadius: '4px',
-    },
-    '.revision-hash': {
-      ...FontsRaw.BodyDefault,
-      marginRight: Spacing.Small,
-    },
-    '.info-caption': {
-      ...captionStylesLight,
-    },
-    '.MuiTypography-root': {
-      ...FontsRaw.BodyDefault,
-    },
-  },
-});
-
-const SelectListDark = style({
-  backgroundColor: Colors.Background300Dark,
-  zIndex: 100,
-  position: 'relative',
-  ...sharedSelectStyles,
-  $nest: {
-    '.MuiListItemButton-root': {
-      padding: `${Spacing.xSmall}px ${Spacing.Small}px`,
-
-      $nest: {
-        '&:hover': {
-          backgroundColor: Colors.SecondaryHoverDark,
-          borderRadius: '4px',
-        },
-        '&:active': {
-          backgroundColor: Colors.SecondaryActiveDark,
-          borderRadius: '4px',
-        },
+      '.item-selected': {
+        backgroundColor: hoverColor,
+        borderRadius: '4px',
+      },
+      '.revision-hash': {
+        ...FontsRaw.BodyDefault,
+        marginRight: Spacing.Small,
+      },
+      '.info-caption': {
+        ...captionStyle,
+      },
+      '.MuiTypography-root': {
+        ...FontsRaw.BodyDefault,
       },
     },
-    '.item-selected': {
-      backgroundColor: Colors.SecondaryHoverDark,
-      borderRadius: '4px',
-    },
-    '.revision-hash': {
-      ...FontsRaw.BodyDefault,
-      marginRight: Spacing.Small,
-    },
-    '.info-caption': {
-      ...captionStylesDark,
-    },
-    '.MuiTypography-root': {
-      ...FontsRaw.BodyDefaultDark,
-    },
-  },
-});
+  });
+}
 
 function SearchResultsList({
   compact,
@@ -112,7 +86,11 @@ function SearchResultsList({
   listItemComponent,
 }: SearchResultsListProps) {
   const mode = useAppSelector((state) => state.theme.mode);
-  const styles = mode === 'light' ? SelectListLight : SelectListDark;
+  const styles = {
+    light: getStyles('light'),
+    dark: getStyles('dark'),
+  };
+  // const styles = mode === 'light' ? SelectListLight : SelectListDark;
 
   const isInProgressChecked = (item: Changeset) => {
     return displayedRevisions.map((rev) => rev.id).includes(item.id);
@@ -120,7 +98,7 @@ function SearchResultsList({
 
   return (
     <Box
-      className={`${styles} results-list-${mode}`}
+      className={`${styles[mode]} results-list-${mode}`}
       id='search-results-list'
       alignItems='flex-end'
       data-testid='list-mode'
