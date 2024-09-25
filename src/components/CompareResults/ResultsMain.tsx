ec6dc0a9c94423cedd7f9f50aef5b6a401d75788
beatrice-acasandrei <69891317+beatrice-acasandrei@users.noreply.github.com>
2024-09-25 16:26:08 +0300

    Add links to the perfcompare app linking to the old compareview, keeping the same revisions (#728)
    * Wip
    
    * Moved links next to Results title
    
    * Address change request
    
    * Small refactor
    
    * Address change request
    
    * Remove comment

--- src/components/CompareResults/ResultsMain.tsx
+++ src/components/CompareResults/ResultsMain.tsx
@@ -1,11 +1,32 @@
+import { Fragment } from 'react';
+
+import { Link } from '@mui/material';
+import Alert from '@mui/material/Alert';
 import { Container } from '@mui/system';
+import { useLoaderData } from 'react-router-dom';
 import { style } from 'typestyle';
 
+import { compareView } from '../../common/constants';
 import { useAppSelector } from '../../hooks/app';
+import {
+  getPerfherderCompareWithBaseViewURL,
+  getPerfherderCompareOverTimeViewURL,
+} from '../../logic/treeherder';
 import { Colors, Spacing } from '../../styles';
+import { truncateHash } from '../../utils/helpers';
+import type { LoaderReturnValue } from './loader';
+import type { LoaderReturnValue as OverTimeLoaderReturnValue } from './overTimeLoader';
 import ResultsTable from './ResultsTable';
 
+function getPunctuationMark(index: number, newRevs: string[]) {
+  return index != newRevs.length - 1 ? ', ' : '.';
+}
+
 function ResultsMain() {
+  const loaderData = useLoaderData() as
+    | LoaderReturnValue
+    | OverTimeLoaderReturnValue;
+
   const themeMode = useAppSelector((state) => state.theme.mode);
 
   const themeColor100 =
@@ -23,10 +44,43 @@ function ResultsMain() {
     }),
   };
 
+  function getCompareViewURL(index: number, rev: string): string | undefined {
+    if (loaderData.view === compareView) {
+      const { frameworkId, baseRepo, baseRev, newRepos } = loaderData;
+      return getPerfherderCompareWithBaseViewURL(
+        baseRepo,
+        baseRev,
+        newRepos[index],
+        rev,
+        frameworkId,
+      );
+    } else {
+      const { frameworkId, baseRepo, newRepos, intervalValue } = loaderData;
+      return getPerfherderCompareOverTimeViewURL(
+        baseRepo,
+        newRepos[index],
+        rev,
+        frameworkId,
+        intervalValue,
+      );
+    }
+  }
+
   return (
     <Container className={styles.container} data-testid='results-main'>
       <header>
-        <div className={styles.title}>Results</div>
+        <div className={styles.title}>Results </div>
+        <Alert severity='info' className={styles.title}>
+          Perfherder links are available for:{' '}
+          {loaderData.newRevs.map((rev, index) => (
+            <Fragment key={rev}>
+              <Link href={getCompareViewURL(index, rev)} target='_blank'>
+                {`comparison ${truncateHash(rev)}`}
+              </Link>
+              {getPunctuationMark(index, loaderData.newRevs)}
+            </Fragment>
+          ))}
+        </Alert>
       </header>
       <ResultsTable />
     </Container>
