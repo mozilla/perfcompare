3e8767c0fe515a5e9b50053c63125da18533f986
Dorcas Bamisile <bamisiledorcas@gmail.com>
2024-10-12 05:53:38 +0100

    fix: Retain filters on refresh

--- src/components/CompareResults/ResultsTable.tsx
+++ src/components/CompareResults/ResultsTable.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -107,20 +107,52 @@ export default function ResultsTable() {
     new Map() as Map<string, Set<string>>, // ColumnID -> Set<Values to remove>
   );
 
-  const onClearFilter = (columnId: string) => {
-    setTableFilters((oldFilters) => {
-      const newFilters = new Map(oldFilters);
-      newFilters.delete(columnId);
-      return newFilters;
+  useEffect(() => {
+    const initialFilters = new Map<string, Set<string>>();
+
+    cellsConfiguration.forEach(({ key, possibleValues }) => {
+      if (!possibleValues) return;
+
+      const paramsValue = rawSearchParams.get(key) ?? possibleValues.join(',');
+      initialFilters.set(
+        key,
+        new Set(possibleValues.filter((v) => !paramsValue.includes(v))),
+      );
+
+      if (!rawSearchParams.has(key)) rawSearchParams.set(key, paramsValue);
     });
+
+    updateRawSearchParams(rawSearchParams);
+    setTableFilters(initialFilters);
+  }, []);
+
+  const onClearFilter = (columnId: string) => {
+    const possibleValues = cellsConfiguration.find(
+      (c) => c.key === columnId,
+    )?.possibleValues;
+
+    if (!possibleValues) return;
+
+    setTableFilters((filters) => (filters.delete(columnId), new Map(filters)));
+    rawSearchParams.set(columnId, possibleValues.join(','));
+    updateRawSearchParams(rawSearchParams);
   };
 
   const onToggleFilter = (columnId: string, filters: Set<string>) => {
-    setTableFilters((oldFilters) => {
-      const newFilters = new Map(oldFilters);
-      newFilters.set(columnId, filters);
-      return newFilters;
-    });
+    const possibleValues = cellsConfiguration.find(
+      (c) => c.key === columnId,
+    )?.possibleValues;
+
+    if (!possibleValues) return;
+
+    const paramsValue =
+      filters.size === possibleValues.length
+        ? ''
+        : possibleValues.filter((v) => !filters.has(v)).join(',');
+
+    setTableFilters((prev) => new Map(prev).set(columnId, filters));
+    rawSearchParams.set(columnId, paramsValue);
+    updateRawSearchParams(rawSearchParams);
   };
 
   const onFrameworkChange = (newFrameworkId: Framework['id']) => {
