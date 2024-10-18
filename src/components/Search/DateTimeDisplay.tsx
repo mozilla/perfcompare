ab47777adbbcf8b8b1c5ef4a238dda366ecf0c4f
Pius Osuji <piusosuji9@gmail.com>
2024-10-18 18:49:47 +0100

    optimize DateTimeDisplay component

--- src/components/Search/DateTimeDisplay.tsx
+++ src/components/Search/DateTimeDisplay.tsx
@@ -1,28 +1,31 @@
 import { Tooltip } from '@mui/material';
 
-const DateTimeDisplay = ({ itemDate }: { itemDate: Date }) => {
-  // Format for local time
-  const localDateTime = itemDate?.toLocaleString('en-US', {
-    year: '2-digit',
-    month: 'short',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: false,
-    timeZoneName: 'short',
-  });
+// Format for local time
+const localDateFormat = new Intl.DateTimeFormat('en-US', {
+  year: '2-digit',
+  month: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: false,
+  timeZoneName: 'short',
+});
 
-  // Format for UTC time
-  const utcDateTime = itemDate?.toLocaleString('en-US', {
-    year: '2-digit',
-    month: 'short',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: false,
-    timeZone: 'UTC',
-    timeZoneName: 'short',
-  });
+// Format for UTC time
+const utcDateFormat = new Intl.DateTimeFormat('en-US', {
+  year: '2-digit',
+  month: 'short',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  hour12: false,
+  timeZone: 'UTC',
+  timeZoneName: 'short',
+});
+
+const DateTimeDisplay = ({ itemDate }: { itemDate: Date }) => {
+  const localDateTime = localDateFormat.format(itemDate);
+  const utcDateTime = utcDateFormat.format(itemDate);
 
   return (
     <Tooltip title={utcDateTime} placement='top'>
