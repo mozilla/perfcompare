1633d54664bae43f89cbd00549ad8bc2f7c8c05a
FatumaA <67555014+FatumaA@users.noreply.github.com>
2024-10-22 22:42:40 +0300

    test: update test definition in ResultsView.test

--- src/__tests__/CompareResults/ResultsView.test.tsx
+++ src/__tests__/CompareResults/ResultsView.test.tsx
@@ -50,7 +50,7 @@ describe('Results View', () => {
     const user = userEvent.setup({ delay: null });
     renderWithRoute(<ResultsView title={Strings.metaData.pageTitle.results} />);
 
-    const header = await screen.findByTestId('results-title-label');
+    const header = await screen.findByTestId('results-title-component');
 
     expect(header).toBeInTheDocument();
 
