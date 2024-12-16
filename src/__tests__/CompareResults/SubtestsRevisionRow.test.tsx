f46d3e9ee0a8c54db2f4311a9549162f6c444f37
Chineta Adinnu <chinetaadinnu@gmail.com>
2024-12-16 13:03:10 +0100

    resolve conflict

---
+++ src/__tests__/CompareResults/SubtestsRevisionRow.test.tsx
@@ -0,0 +1,75 @@
+import { ReactElement } from 'react';
+
+import { loader } from '../../components/CompareResults/loader';
+import SubtestsRevisionRow from '../../components/CompareResults/SubtestsResults/SubtestsRevisionRow';
+import getTestData from '../utils/fixtures';
+import {
+  screen,
+  renderWithRouter,
+  FetchMockSandbox,
+} from '../utils/test-utils';
+
+function renderWithRoute(component: ReactElement) {
+  (window.fetch as FetchMockSandbox)
+    .get('begin:https://treeherder.mozilla.org/api/perfcompare/results/', [])
+    .get('begin:https://treeherder.mozilla.org/api/project/', {
+      results: [],
+    });
+
+  return renderWithRouter(component, {
+    route: '/subtests-compare-results/',
+    search: '?baseRev=spam&baseRepo=try&framework=1',
+    loader,
+  });
+}
+
+describe('SubtestsRevisionRow Component', () => {
+  it('renders the component with correct data', async () => {
+    const { subtestsResult } = getTestData();
+    const mockGridTemplateColumns = '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr';
+    renderWithRoute(
+      <SubtestsRevisionRow
+        result={subtestsResult[0]}
+        gridTemplateColumns={mockGridTemplateColumns}
+      />,
+    );
+
+    // Check if all elements render correctly
+    await screen.findByText('dhtml.html');
+    expect(await screen.findByText('971.38 ms')).toBeInTheDocument();
+    expect(await screen.findByText('982.41 ms')).toBeInTheDocument();
+    expect(await screen.findByText('1.14 %')).toBeInTheDocument();
+    expect(await screen.findByText('Low')).toBeInTheDocument();
+    expect(document.body).toMatchSnapshot();
+  });
+
+  it('renders the Regression status with bg color', async () => {
+    const { subtestsResult } = getTestData();
+    const mockGridTemplateColumns = '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr';
+    renderWithRoute(
+      <SubtestsRevisionRow
+        result={subtestsResult[2]}
+        gridTemplateColumns={mockGridTemplateColumns}
+      />,
+    );
+
+    // Check if all elements render correctly
+    await screen.findByText('regression.html');
+    expect(await screen.findByText('Regression')).toBeInTheDocument();
+  });
+
+  it('renders the Improvement status with bg color', async () => {
+    const { subtestsResult } = getTestData();
+    const mockGridTemplateColumns = '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr';
+    renderWithRoute(
+      <SubtestsRevisionRow
+        result={subtestsResult[3]}
+        gridTemplateColumns={mockGridTemplateColumns}
+      />,
+    );
+
+    // Check if all elements render correctly
+    await screen.findByText('improvement.html');
+    expect(await screen.findByText('Improvement')).toBeInTheDocument();
+  });
+});
