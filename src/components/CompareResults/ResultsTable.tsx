6d4b6d3361ac49e0529fd9529c789a18bbf798ce
Julien Wajsberg <felash@gmail.com>
2025-01-09 14:48:58 +0100

    Rename OSX to macOS to better reflect the newest naming for this platform

--- src/components/CompareResults/ResultsTable.tsx
+++ src/components/CompareResults/ResultsTable.tsx
@@ -25,7 +25,7 @@ const columnsConfiguration: CompareResultsTableConfig = [
     gridWidth: '2fr',
     possibleValues: [
       { label: 'Windows', key: 'windows' },
-      { label: 'OSX', key: 'osx' },
+      { label: 'macOS', key: 'osx' },
       { label: 'Linux', key: 'linux' },
       { label: 'Android', key: 'android' },
     ],
