81de371fa472bc2cfd73c04eae7b89fab9c4327d
Carla Severe <csevere@mozilla.com>
2025-01-27 16:53:09 -0800

    added the edit icon beside results page title

--- src/components/Shared/PerfCompareHeader.tsx
+++ src/components/Shared/PerfCompareHeader.tsx
@@ -7,6 +7,8 @@ import ToggleDarkMode from './ToggleDarkModeButton';
 import { useAppSelector } from '../../hooks/app';
 import { Strings } from '../../resources/Strings';
 import { HeaderStyles } from '../../styles';
+import pencilDark from '../../theme/img/pencil-dark.svg';
+import pencil from '../../theme/img/pencil.svg';
 
 interface PerfCompareHeaderProps {
   isHome?: boolean;
@@ -17,19 +19,54 @@ const strings = Strings.components.header;
 function PerfCompareHeader({ isHome }: PerfCompareHeaderProps) {
   const themeMode = useAppSelector((state) => state.theme.mode);
   const styles = HeaderStyles(themeMode, isHome ?? false);
+  const homePageSx = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    maxWidth: '472px',
+    margin: '0 auto',
+  };
+
+  const resultPageSx = {
+    ...homePageSx,
+    flexDirection: 'row',
+  };
+
+  const buttonIcon = (
+    <img
+      id='edit-title-icon'
+      className='icon icon-edit'
+      src={themeMode === 'light' ? pencil.toString() : pencilDark.toString()}
+      alt='edit-icon'
+    />
+  );
 
   return (
     <Grid className={`header-container ${styles.container}`}>
       <ToggleDarkMode />
-      <Box className='header-text'>
+      <Box className='header-text' sx={isHome ? homePageSx : resultPageSx}>
         <Typography
           variant='h1'
           align='center'
           gutterBottom
+          pr={isHome ? '0' : 1}
           className='perfcompare-header'
         >
           {strings.title}
         </Typography>
+        {isHome ? (
+          ''
+        ) : (
+          <Button
+            name='edit-title'
+            aria-label='edit title'
+            startIcon={buttonIcon}
+            className='start-btn'
+            variant='text'
+          >
+            Edit title
+          </Button>
+        )}
         {isHome ? (
           <>
             <Typography
