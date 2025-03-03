2ef725bb27108e19c2f555d09550559c480a0e5b
Chineta Adinnu <chinetaadinnu@gmail.com>
2025-03-03 17:37:17 +0100

    add browser name to result header for main results and subtests

--- src/components/CompareResults/RevisionRow.tsx
+++ src/components/CompareResults/RevisionRow.tsx
@@ -24,6 +24,7 @@ import { formatNumber } from '../../utils/format';
 import {
   getPlatformShortName,
   getPlatformAndVersion,
+  getBrowserDisplay,
 } from '../../utils/platform';
 import AndroidIcon from '../Shared/Icons/AndroidIcon';
 import LinuxIcon from '../Shared/Icons/LinuxIcon';
@@ -109,6 +110,13 @@ const stylesLight = {
         // We need to move the icon a bit lower so that it _looks_ centered.
         marginTop: '2px',
       },
+      '.browser-name': {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        padding: '10px 0px',
+      },
     },
   }),
   typography: style({
@@ -180,6 +188,13 @@ const stylesDark = {
       '.status-hint-regression .MuiSvgIcon-root': {
         // We need to move the icon a bit lower so that it _looks_ centered.
         marginTop: '2px',
+      },
+      '.browser-name': {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        padding: '10px 0px',
       },
     },
   }),
@@ -281,6 +296,8 @@ function RevisionRow(props: RevisionRowProps) {
     base_runs: baseRuns,
     new_runs: newRuns,
     graphs_link: graphLink,
+    base_app: baseApp,
+    new_app: newApp,
   } = result;
 
   const platformShortName = getPlatformShortName(platform);
@@ -322,16 +339,20 @@ function RevisionRow(props: RevisionRowProps) {
             </div>
           </Tooltip>
         </div>
-        <div className='base-value cell' role='cell'>
-          {' '}
-          {formatNumber(baseAvgValue)} {baseUnit}{' '}
+        <div className='browser-name cell' role='cell'>
+          {formatNumber(baseAvgValue)} {baseUnit}
+          {getBrowserDisplay(baseApp, newApp, expanded) && (
+            <span style={{ fontSize: '11.5px' }}>({baseApp})</span>
+          )}
         </div>
         <div className='comparison-sign cell' role='cell'>
           {determineSign(baseAvgValue, newAvgValue)}
         </div>
-        <div className='new-value cell' role='cell'>
-          {' '}
+        <div className='browser-name cell' role='cell'>
           {formatNumber(newAvgValue)} {newUnit}
+          {getBrowserDisplay(baseApp, newApp, expanded) && (
+            <span style={{ fontSize: '11.5px' }}>({newApp})</span>
+          )}
         </div>
         <div className='status cell' role='cell'>
           <Box
