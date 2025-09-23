e12760ee3292a0883b7c28dc80b32a8e083f8cf1
Vi Tran <vipran@gmail.com>
2025-09-23 12:17:33 -0400

    Add test version selection dropdown

---
+++ src/components/Shared/TestVersionDropdown.tsx
@@ -0,0 +1,55 @@
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { ThemeMode } from '../../types/state';
+
+interface TestVersionDropdownProps {
+  testType: string;
+  size?: 'small' | 'medium';
+  variant?: 'standard' | 'outlined' | 'filled';
+  mode: ThemeMode;
+  onChange: (test_version: string) => void;
+}
+
+const TEST_VERSIONS = [{type: 'mann-whitney-u', label: 'Mann Whitney U Test'}, {type: 'student-t', label: 'Student-T Test'}]
+
+function TestVersionDropdown({
+  testType,
+  variant,
+  size,
+  onChange,
+  mode,
+}: TestVersionDropdownProps) {
+  const onValueChange = (event: SelectChangeEvent) => {
+    const test_version = event.target.value;
+    onChange(test_version);
+  };
+
+  return (
+    <Select
+      data-testid='framework-select'
+      value={testType}
+      labelId={'test-version-type'}
+      className='framework-dropdown-select'
+      onChange={onValueChange}
+      name='framework'
+      variant={variant}
+      size={size}
+      MenuProps={{
+        classes: {
+          paper: `paper-repo paper-${mode === 'light' ? 'light' : 'dark'}`,
+        },
+      }}
+      inputProps={{
+        'aria-label': 'Statistics Test Type',
+      }}
+    >
+      {TEST_VERSIONS.map(({label, type}) => (
+        <MenuItem value={type} key={type} className={`statistic-test-item`}>
+          {label}
+        </MenuItem>
+      ))}
+    </Select>
+  );
+}
+
+export default TestVersionDropdown;
