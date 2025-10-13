64ad9eef7aaef6d99a213a70dce22c8130e8dd32
Vi Tran <vipran@gmail.com>
2025-10-13 16:13:24 -0400

    fix linting issues

--- src/components/CompareResults/StatisticsWarnings.tsx
+++ src/components/CompareResults/StatisticsWarnings.tsx
@@ -1,7 +1,6 @@
 import { Warning } from '@mui/icons-material';
 import { Box } from '@mui/system';
 
-import { useAppSelector } from '../../hooks/app';
 import { Colors } from '../../styles';
 import { MannWhitneyResultsItem } from '../../types/state';
 
@@ -10,29 +9,23 @@ export const StatisticsWarnings = ({
 }: {
   result: MannWhitneyResultsItem;
 }) => {
-  function getStyles(theme: string) {
-    return {
-      backgroundColor: 'transparent',
-      marginTop: 2,
-      marginBottom: 2,
+  const componentStyles = {
+    backgroundColor: 'transparent',
+    marginTop: 2,
+    marginBottom: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    borderRadius: 1,
+    padding: 2,
+    '& .warning-row': {
+      verticalAlign: 'bottom',
       display: 'flex',
-      flexDirection: 'column',
-      flexWrap: 'wrap',
-      borderRadius: 1,
-      padding: 2,
-      '& .warning-row': {
-        verticalAlign: 'bottom',
-        display: 'flex',
-        marginTop: 1,
-        marginBottom: 1,
-      },
-    };
-  }
-  const mode = useAppSelector((state) => state.theme.mode);
-  const styles = {
-    light: getStyles('light'),
-    dark: getStyles('dark'),
+      marginTop: 1,
+      marginBottom: 1,
+    },
   };
+
   // Combine all warnings into a single list
   const warnings: string[] = [
     ...(result?.shapiro_wilk_warnings ?? []),
@@ -43,7 +36,7 @@ export const StatisticsWarnings = ({
   return (
     <>
       {warnings.length ? (
-        <Box sx={{ ...styles[mode] }}>
+        <Box sx={componentStyles}>
           {warnings.map((warning) => (
             <span key={warning} className='warning-row'>
               <Warning sx={{ color: Colors.WarningIcon, marginRight: 1 }} />{' '}
