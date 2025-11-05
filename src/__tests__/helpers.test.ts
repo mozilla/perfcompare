44d177cffab4b8d58e7e47f3bfad7ecbec5c776e
Vi Tran <vipran@gmail.com>
2025-11-05 07:59:34 -0500

    add get mode interpretation and helper test

--- src/__tests__/helpers.test.ts
+++ src/__tests__/helpers.test.ts
@@ -5,6 +5,7 @@ import {
   truncateHash,
   swapArrayElements,
   getDocsURL,
+  getModeInterpretation,
 } from '../utils/helpers';
 import getTestData from './utils/fixtures';
 
@@ -118,5 +119,39 @@ describe('swapArrayElements', () => {
   it('should return initial copy of inital array', () => {
     const swappedArray = swapArrayElements(array, 5, 0);
     swappedArray.forEach((el, index) => expect(el).toEqual(array[index]));
+  });
+});
+
+describe('getModeInterpretation', () => {
+  const baseMode1 = 1;
+  const newMode1 = 1;
+  const baseMode2 = 2;
+  const newMode2 = 2;
+  const baseMode0 = null;
+  const newMode0 = 0;
+
+  it('should handle same mode count interpretation for base and new', () => {
+    const expectedStr = getModeInterpretation(baseMode1, newMode1);
+    expect(expectedStr).toBe('Base and New revisions are unimodal');
+    const expectedStr2 = getModeInterpretation(baseMode2, newMode2);
+    expect(expectedStr2).toBe('Base and New revisions are multimodal');
+  });
+
+  it('should handle different mode count interpretation for base and new', () => {
+    const expectedStr2 = getModeInterpretation(baseMode2, newMode1);
+    expect(expectedStr2).toBe('Base is multimodal and New is unimodal');
+    const expectedStr3 = getModeInterpretation(baseMode1, newMode2);
+    expect(expectedStr3).toBe('Base is unimodal and New is multimodal');
+  });
+
+  it('should get mode interpretation with 0 or null mode count', () => {
+    const expectedStr4 = getModeInterpretation(baseMode0, newMode0);
+    expect(expectedStr4).toBe(
+      'No modes or data for Base and New, possible oversmoothing, KDE evaluation failed',
+    );
+    const expectedStr0 = getModeInterpretation(baseMode0, newMode1);
+    expect(expectedStr0).toBe('Base is N/A and New is unimodal');
+    const expectedStr1 = getModeInterpretation(baseMode2, newMode0);
+    expect(expectedStr1).toBe('Base is multimodal and New is N/A');
   });
 });
