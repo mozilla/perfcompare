6b0279e397737351c0d6711a2b077dfbfc4eb865
Alexander Moses <moijes12@gmail.com>
2026-02-03 04:37:04 +0000

    Bug-2003016: Use MUI spacing on Box element
    Implemented the review comments by:
    * Using MUI space of `mb: 3` instead of the `styles`. This will
    have the styles match the original.
    
    I have also regenerated the screenshots by running `npm run fix-all`.

--- src/components/Shared/LinkToHome.tsx
+++ src/components/Shared/LinkToHome.tsx
@@ -2,19 +2,10 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import { Box } from '@mui/system';
-import { style } from 'typestyle';
-
-import { Spacing } from '../../styles';
 
 export function LinkToHome() {
-  const styles = {
-    box: style({
-      marginBottom: `${Spacing.Small + 1}px`,
-    }),
-  };
-
   return (
-    <Box className={styles.box}>
+    <Box sx={{ mb: 3 }}>
       <Link href='/' aria-label='link to home' sx={{ display: 'inline-flex' }}>
         <Stack
           direction='row'
