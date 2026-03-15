9efab83dd0200f8841967c70805dad3321b8478a
Alexander Moses <moijes12@gmail.com>
2026-03-15 08:53:08 +0000

    Bug-2022758: Fix lint errors
    * Cleanup lint errors in `SearchContainerStyles.ts`
    * Fix snapshots

--- src/styles/SearchContainerStyles.ts
+++ src/styles/SearchContainerStyles.ts
@@ -3,8 +3,6 @@ import { stylesheet } from 'typestyle';
 import { FontsRaw, Spacing } from '../styles';
 
 export const SearchContainerStyles = (mode: string, isHome: boolean) => {
-  const isTrueLight = mode == 'light';
-
   const styles = stylesheet({
     container: {
       /*** maxWidth based on mozilla protocol large cards size; see https://protocol.mozilla.org/components/detail/card--large
